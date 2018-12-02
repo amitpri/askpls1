b8c8008a6605f2d73fb851369974ee66c9cc4c1a
Amit Priyadarshi <amitpri@gmail.com>
2018-12-02 11:08:25 +0530

    Footer changes

--- portal/resources/views/layout.blade.php
+++ portal/resources/views/layout.blade.php
@@ -56,11 +56,10 @@
                     @yield('content')
 
                     <p class="mt-8 text-center text-xs text-80">
-                        <a href="http://nova.laravel.com" class="text-primary dim no-underline">Laravel Nova</a>
+                         
                         <span class="px-1">&middot;</span>
-                        &copy; {{ date('Y') }} Laravel LLC - By Taylor Otwell, David Hemphill, and Steve Schoger.
-                        <span class="px-1">&middot;</span>
-                        v{{ Laravel\Nova\Nova::version() }}
+                        &copy; {{ date('Y') }} AskPls - Private Anonymous Review System. All rights reserved
+                         
                     </p>
                 </div>
             </div>
